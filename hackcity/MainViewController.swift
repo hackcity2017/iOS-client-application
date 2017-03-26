18f39b44c22164a3e4c44627f53552f7a527f7eb
lvh1g15 <landon2008@hotmail.com>
2017-03-26 04:20:30 +0100

    adding ID to database

--- hackcity/MainViewController.swift
+++ hackcity/MainViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.performSegue(withIdentifier: "mainviewmodal", sender: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
+        
         // Do any additional setup after loading the view.
     }
 
@@ -20,16 +27,6 @@ class MainViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- hackcity/SignIdViewController.swift
+++ hackcity/SignIdViewController.swift
@@ -7,29 +7,69 @@
 //
 
 import UIKit
+import Alamofire
 
 class SignIdViewController: UIViewController {
 
+    @IBOutlet weak var inputIDField: UITextField!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
     
+    @IBAction func connect(_ sender: Any) {
+        
+        if inputIDField.text == "" {
+            
+            let alertController = UIAlertController(title: "Oops!!", message: "Must enter an ID to continue 🤓", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            
+            //login
+            let id = inputIDField.text
+            let idDict = [
+                "deviceId": id
+            ]
+            
+            let post = Alamofire.request("http://172.16.0.49:4242/device", method: .post, parameters: idDict)
+            post.validate().responseJSON(completionHandler: {response in
+                switch response.result {
+                case .success:
+                    print("Validation Successful")
+                    DispatchQueue.main.async {
+                        self.dismiss(animated: true, completion: nil)                        
+                    }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+                    
+                case .failure(let error):
+                    print(error)
+                    
+                    let alertController = UIAlertController(title: "Oops!!", message: "failure to load data", preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                }
+            })
+        }
     }
-    */
-
+    
 }
+
+
+
+
+
+
+
+
+
